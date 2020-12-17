6a4c0a83b8a30c836c28297564d81750a52e8e27
phil <pgrenon.pg@gmail.com>
2020-12-17 09:50:17 -0500

    re-init

---
+++ index.js
@@ -0,0 +1,19 @@
+const { Client, MessageAttachment } = require('discord.js');
+const config = require('./config.json');
+
+const client = new Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.author.id != client.user.id) {
+    if (msg.content.match(/(^|\W)-crash(\W|$)/gi)) {
+      msg.channel.send("test");
+      return;
+    }
+  }
+});
+
+client.login(config.token);
