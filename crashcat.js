9d6e6b041c03d5aa4cf6f10178826d0ec430dea2
Phil <pgrenon.pg@gmail.com>
2022-03-21 21:54:30 -0400

    hell if i know

--- crashcat.js
+++ crashcat.js
@@ -1,5 +1,6 @@
 'use strict';
 require('dotenv').config();
+require(`discord-reply`);
 const schedule = require('node-schedule');
 const fetch = require("node-fetch");
 const { Client, MessageAttachment, Message } = require('discord.js');
@@ -10,8 +11,12 @@ const SteamAPI = require('steamapi');
 const steam = new SteamAPI('61D686B6252F0A5ABC2673AB5B8D748B');
 const util = require('util');
 const asyncJsonReader = util.promisify(jsonReader);
+const pollEmbed = require('discord.js-poll-embed');
+const discordEmojiRegex = /(<a?)?:\w+:(\d{18}>)?/g;
+const unicodeEmojiRegex = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g;
 
-const wishlistPath = './wishlist.json';
+const dataPath = './data.json';
+let data;
 const prefix = `-`;
 const crashCommand = "crash";
 const complimentURL = 'https://complimentr.com/api';
@@ -42,16 +47,34 @@ const client = new Client();
 
 client.on('ready', () =>
 {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`\nLogged in as ${client.user.tag}!`);
+    loadData();
     schedule.scheduleJob('0 12 * * *', () => { checkWishlists(); });
+    schedule.scheduleJob('0 0 10 ? * * *', () => { pollIcon(); });
 });
+
+async function loadData()
+{
+    data = await asyncJsonReader(dataPath);
+}
+
+function setupChannel(guild, channelID)
+{
+    data.channels[guild] = channelID;
+    writeData();
+}
+
+function pollIcon()
+{
+
+}
 
 client.on('message', msg =>
 {
-    if (msg.author.id == client.user.id || !msg.content.startsWith(prefix) || msg.author.bot)
+    if (msg.author.id == client.user.id || !msg.content.startsWith(prefix))
         return;
 
-    console.log(`COMMAND RECEIVED : ${msg.content}`);
+    console.log(`COMMAND RECEIVED: ${msg.content}`);
 
     let msgContent = msg.content;
 
@@ -64,10 +87,11 @@ client.on('message', msg =>
         msgContent = msgContent.replace(stringParam, "");
     }
 
-    const args = msgContent.slice(prefix.length).trim().split(' ');
-    console.log(`ARGUMENTS : ${args}`);
+    const command = msgContent.slice(prefix.length).trim().split(' ')[0];
+    console.log(`COMMAND TYPE: ${command[0]}`);
 
-    const command = args[0];
+    const paramsString = msgContent.substring(msgContent.indexOf(' ')).trim().trimStart();
+    console.log(`PARAMS STRING: ${paramsString}`);
 
     if (command.startsWith(crashCommand))
     {
@@ -85,6 +109,41 @@ client.on('message', msg =>
     else if (command.startsWith("why"))
     {
         msg.channel.send("```TIL Research shows that viewing online Cat media(i.e.pictures and videos) is related to positive emotions.It may even work as a form of digital therapy or stress relief for some users.Some feelings of guilt from postponing tasks can also be reduced by viewing Cat content.``` https://www.reddit.com/r/todayilearned/comments/m0giw8/til_research_shows_that_viewing_online_cat_media/?utm_medium=android_app&utm_source=share");
+    }
+    else if (command.startsWith("setup"))
+    {
+        setupChannel(msg.guild.id, msg.channel.id);
+        msg.channel.send(`\`\`\`Updated prefered channel: ${msg.channel.name}\`\`\``);
+    }
+    else if (command.startsWith("poll"))
+    {
+        if (!paramsString.includes(`?`))
+        {
+            msg.channel.send(`\`\`\`Bad format. Try something like : -poll What is your favorite food ? Pasta + Burgers + Pizza\`\`\``)
+            return;
+        }
+
+        const question = paramsString.substring(0, paramsString.indexOf(`?`));
+        console.log(`POLL QUESTION: ` + question);
+
+        const afterQuestion = paramsString.substring(paramsString.indexOf(`?`) + 1).trim();
+
+        const pollParams = afterQuestion.split(';');
+        const pollOptions = pollParams[0].trim().split('+').map(item => item.replace('+', '').trim());
+        console.log(`POLL OPTIONS(${pollOptions.length}): ` + pollOptions);
+
+        let emojis = undefined;
+        if (pollParams[1] != undefined)
+            emojis = pollParams[1].trim().split(/[\s,]+/);
+        else if (pollOptions.every(option => option.match(discordEmojiRegex) || option.match(unicodeEmojiRegex)))
+            emojis = pollOptions.map(item => item.replace('+', '').trim());
+        console.log(`EMOJIS(${emojis != undefined ? emojis.length : 0}): ` + emojis);
+
+        let timeout = 0;
+        if (pollParams[2] != undefined && isNumeric(pollParams))
+            timeout =
+
+                pollEmbed(msg, question, pollOptions, timeout, emojis, undefined);
     }
     else if (command.startsWith("wish") || command.startsWith("w"))
     {
@@ -122,9 +181,52 @@ client.on('message', msg =>
     }
 });
 
+function pollCallback(msg, emojiInfo)
+{
+    let highestVoteCount = 0;
+    let winner;
+    const results = Object.values(emojiInfo);
+    for (let i = 0; i < results.length; i++)
+    {
+        const result = results[i];
+        if (result.votes > highestVoteCount)
+        {
+            highestVoteCount = result.votes;
+            winner = result;
+        }
+    }
+
+    if (winner == undefined) // zero votes
+    {
+        msg.lineReply("No one voted! Booooooo!");
+        return;
+    }
+
+    let winners = [];
+    results.forEach(result =>
+    {
+        if (result.votes == winner.votes)
+            winners.push(result);
+    });
+
+    if (winners.length > 1) // exequo
+    {
+        let text = "Exequo between `"
+        for (const exequoWinner of winners)
+        {
+            text += `${exequoWinner.option} `;
+        }
+        msg.lineReply(text);
+        return;
+    }
+
+    msg.lineReply(`Poll Winner is ${winner.option}!`);
+}
+
 async function checkWishlists()
 {
-    const wishlists = await asyncJsonReader(wishlistPath);
+    const data = await asyncJsonReader(dataPath);
+    const wishlists = data.wishlists;
     for (let [guild, wishlist] of Object.entries(wishlists))
     {
         let channel = wishlist.channel;
@@ -168,17 +270,22 @@ async function postRandomUrlFromSubreddit(sub, msg)
         url = post[5].url
     }
     console.log(url);
+
     msg.channel.send(post.url);
+
+    msg.react("<:patpatunity:869975028421251082>");
 }
 
 async function readWishlist(msg, guild)
 {
-    let wishlist = await asyncJsonReader(wishlistPath);
-    if (wishlist[guild])
+    let data = await asyncJsonReader(dataPath);
+    let wishlists = data.wishlists;
+    console.log(wishlists);
+    if (wishlists[guild])
     {
         let content = "\n";
         let i = 1;
-        for (let [appName, appEntry] of Object.entries(wishlist[guild]))
+        for (let [appName, appEntry] of Object.entries(wishlists[guild]))
         {
             if (!appEntry.hasOwnProperty("appid"))
                 continue;
@@ -240,7 +347,7 @@ async function replyCrash(crashSuffix, msg)
             postRandomUrlFromSubreddit('cutecats', msg);
             break;
         case "d":
-            postRandomUrlFromSubreddit('doggos', msg);
+            postRandomUrlFromSubreddit('rarepuppers', msg);
             break;
         case "a":
             postRandomUrlFromSubreddit('babyanimals', msg);
@@ -250,10 +357,6 @@ async function replyCrash(crashSuffix, msg)
             break;
         case "b":
             postRandomUrlFromSubreddit('birbs', msg);
-            break;
-        case "snek":
-            msg.reply("crashsnek is deprecated, please use crashs lol but here is your snek anyway :)");
-            postRandomUrlFromSubreddit('sneks', msg);
             break;
         case "s":
             postRandomUrlFromSubreddit('sneks', msg);
@@ -279,20 +382,21 @@ async function addToWishlist(msg, guild, appName, percent = 100)
     {
         if (appEntry.name == appName)
         {
-            let wishlist = await asyncJsonReader(wishlistPath);
+            let data = await asyncJsonReader(dataPath);
 
-            if (!wishlist[guild])
-                wishlist[guild] = {};
+            let wishlists = data.wishlists;
+            if (!wishlists[guild])
+                wishlists[guild] = {};
 
             appEntry["discount"] = percent;
 
             appEntry["last_announcement"] = 100;
 
-            wishlist[guild][appName] = appEntry;
+            wishlists[guild][appName] = appEntry;
 
-            wishlist = updateWishlistChannel(wishlist, guild, msg.channel);
+            wishlists = updateWishlistChannel(wishlists, guild, msg.channel);
 
-            writeWishlist(wishlist);
+            writeWishlist(wishlists);
 
             msg.channel.send("Added to wishlist " + guild.name + ": " + appName + " at <" + percent + "%");
         }
@@ -301,7 +405,7 @@ async function addToWishlist(msg, guild, appName, percent = 100)
 
 async function removeFromWishlist(msg, guild, appName)
 {
-    let wishlist = await asyncJsonReader(wishlistPath);
+    let wishlist = await asyncJsonReader(dataPath);
 
     delete wishlist[guild][appName];
 
@@ -324,6 +428,15 @@ function updateWishlistChannel(wishlist, guild, channel)
 function writeWishlist(wishlists)
 {
     fs.writeFile('./wishlist.json', JSON.stringify(wishlists, null, 4), (err) =>
+    {
+        if (err)
+            console.log('Error writing file:', err);
+    });
+}
+
+function writeData()
+{
+    fs.writeFile('./data.json', JSON.stringify(data, null, 4), (err) =>
     {
         if (err)
             console.log('Error writing file:', err);
